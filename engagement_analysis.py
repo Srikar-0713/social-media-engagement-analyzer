39b3d22d3ae5d97e72ec5a25e7ec12f2fff75c35
Srikar-0713 <srikar.andhavarapu07@gmail.com>
2025-12-27 10:25:03 +0530

    Update engagement_analysis.py

--- engagement_analysis.py
+++ engagement_analysis.py
@@ -6,9 +6,11 @@ data = pd.read_csv("social_media_data.csv")
 print("Social Media Engagement Data:")
 print(data)
 
-plt.figure()
+plt.figure(figsize=(6,4))
 plt.bar(data["Post"], data["Likes"])
 plt.xlabel("Post")
 plt.ylabel("Likes")
 plt.title("Likes per Post")
+
+plt.tight_layout()
 plt.show()
